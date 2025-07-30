ef779cd9f55d8ab1a69505c2c59230ff25358989
EndOverdosing <endoverdosing@gmail.com>
2025-07-30 01:22:33 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from .database import Base
+from database import Base
 
 class URL(Base):
     __tablename__ = "urls"
